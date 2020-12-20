f0b40e8716e79d6ba7cc7c0cc302224f20da74f8
Gabriel Jahbez <45593692+gabrieljmw@users.noreply.github.com>
2020-12-20 16:36:22 -0500

    Implementing temp audio progress code

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
+//TODO: fix audio counter to display normally
+
 let audio1 = new Audio('audio/taa-pet.mp3');
 let play = document.querySelector('.play');
 let pause = document.querySelector('.pause');
 
 play.addEventListener('click', () => {
     audio1.play();
-    document.querySelector('.tool.play').classList.add("hidden");
-    document.querySelector('.tool.pause').classList.remove("hidden");
+    let playIcon = document.querySelector('.tool.play');
+    let pauseIcon = document.querySelector('.tool.pause');
+    playIcon.classList.add("hidden");
+    pauseIcon.classList.remove("hidden");
     audio1.addEventListener('timeupdate', () => {
-        document.querySelector('progress').setAttribute("value", audio1.currentTime);
-        document.querySelector('progress').setAttribute("max", audio1.duration);
+        let progress = document.querySelector('progress');
+        progress.setAttribute("value", audio1.currentTime);
+        progress.setAttribute("max", audio1.duration);
+        let currentTimeVal = document.querySelector('.progress-text .currentTime');
+        let maxDuration = document.querySelector('.progress-text .duration');
+        let s = (audio1.currentTime % 60);
+        let m = (audio1.currentTime / 60);
+        let durationVal = Math.floor(audio1.duration / 60) + ":" + Math.ceil(audio1.duration % 60);
+        currentTimeVal.innerHTML = `${Math.floor(m)}:${Math.ceil(s)}`;
+        maxDuration.innerHTML = durationVal;
+
     })
 });
 
