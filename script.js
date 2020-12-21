8891b55dc826b384c89b799aa6fd301b63c27c47
Gabriel Jahbez <45593692+gabrieljmw@users.noreply.github.com>
2020-12-20 21:45:07 -0500

    New audio implementation

--- script.js
+++ script.js
@@ -1,32 +1,81 @@
 //TODO: fix audio counter to display normally
 
-let audio1 = new Audio('audio/taa-pet.mp3');
-let play = document.querySelector('.play');
-let pause = document.querySelector('.pause');
+// let audio1 = new Audio('audio/taa-pet.mp3');
+// let audio2 = new Audio('audio/jazz-club.mp3');
+// let audio3 = new Audio('audio/comfort-fit.mp3');
+// let audio4 = new Audio('audio/nobara.mp3');
+// let audio5 = new Audio('audio/paper-navy.mp3');
 
-play.addEventListener('click', () => {
-    audio1.play();
-    let playIcon = document.querySelector('.tool.play');
-    let pauseIcon = document.querySelector('.tool.pause');
-    playIcon.classList.add("hidden");
-    pauseIcon.classList.remove("hidden");
-    audio1.addEventListener('timeupdate', () => {
-        let progress = document.querySelector('progress');
-        progress.setAttribute("value", audio1.currentTime);
-        progress.setAttribute("max", audio1.duration);
-        let currentTimeVal = document.querySelector('.progress-text .currentTime');
-        let maxDuration = document.querySelector('.progress-text .duration');
-        let s = (audio1.currentTime % 60);
-        let m = (audio1.currentTime / 60);
-        let durationVal = Math.floor(audio1.duration / 60) + ":" + Math.ceil(audio1.duration % 60);
-        currentTimeVal.innerHTML = `${Math.floor(m)}:${Math.ceil(s)}`;
-        maxDuration.innerHTML = durationVal;
+let audioList = {
+    audio1: new Audio('audio/taa-pet.mp3'),
+    audio2: new Audio('audio/jazz-club.mp3'),
+    audio3: new Audio('audio/comfort-fit.mp3'),
+    audio4: new Audio('audio/nobara.mp3'),
+    audio5: new Audio('audio/paper-navy.mp3')
+};
 
+let audioListLen = Object.keys(audioList).length;
+let playable = document.querySelectorAll('.fas.play');
+
+playable.forEach((item) => {
+    item.addEventListener('click', () => {
+        for (let i = 0; i < audioListLen; ++i) {
+            if (item.getAttribute('data-audio') == Object.keys(audioList)[i]) {
+                item
+                Object.values(audioList)[i].play();
+            }
+        }
     })
-});
+})
 
-pause.addEventListener('click', () => {
-    document.querySelector('.tool.pause').classList.add("hidden");
-    document.querySelector('.tool.play').classList.remove("hidden");
-    audio1.pause();
-});
+
+
+
+// let play = document.querySelector('.play');
+// let pause = document.querySelector('.pause');
+
+// play.addEventListener('click', () => {
+//     audio1.play();
+//     let playIcon = document.querySelector('.tool.play');
+//     let pauseIcon = document.querySelector('.tool.pause');
+//     playIcon.classList.add("hidden");
+//     pauseIcon.classList.remove("hidden");
+//     audio1.addEventListener('timeupdate', () => {
+//         let progress = document.querySelector('progress');
+//         progress.setAttribute("value", audio1.currentTime);
+//         progress.setAttribute("max", audio1.duration);
+//         let currentTimeVal = document.querySelector('.progress-text .currentTime');
+//         let maxDuration = document.querySelector('.progress-text .duration');
+//         let s = (audio1.currentTime % 60);
+//         let m = (audio1.currentTime / 60);
+//         let durationVal = Math.floor(audio1.duration / 60) + ":" + Math.ceil(audio1.duration % 60);
+//         currentTimeVal.innerHTML = `${Math.floor(m)}:${Math.ceil(s)}`;
+//         maxDuration.innerHTML = durationVal;
+
+//     })
+// });
+
+
+
+
+// let audio1 = new Audio('audio/taa-pet.mp3');
+// let audio2 = new Audio('audio/jazz-club.mp3');
+// let audio3 = new Audio('audio/comfort-fit.mp3');
+// let audio4 = new Audio('audio/nobara.mp3');
+// let audio5 = new Audio('audio/paper-navy.mp3');
+
+// let playAudio1 = document.querySelector('[data-audio="1"]');
+// let pauseAudio1 = document.querySelector('[data-audio="1p"]');
+
+// playAudio1.addEventListener('click', () => {
+//     audio1.play();
+//     playAudio1.classList.add('hidden');
+//     pauseAudio1.classList.remove('hidden');
+// })
+
+
+// playAudio1.addEventListener('click', () => {
+//     audio1.play();
+//     playAudio1.classList.add('hidden');
+//     pauseAudio1.classList.remove('hidden');
+// })
