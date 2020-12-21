bfb471cbfdc9743c51ed55bbd862ba97d9d1b721
Gabriel Jahbez <45593692+gabrieljmw@users.noreply.github.com>
2020-12-21 00:30:57 -0500

    -

--- script.js
+++ script.js
@@ -4,78 +4,84 @@
         fix bug where if song is already playing, shuffle will overlap it
         */
 
-let audioList = {
-    audio1: new Audio('audio/taa-pet.mp3'),
-    audio2: new Audio('audio/jazz-club.mp3'),
-    audio3: new Audio('audio/comfort-fit.mp3'),
-    audio4: new Audio('audio/nobara.mp3'),
-    audio5: new Audio('audio/paper-navy.mp3')
-};
+window.addEventListener('load', function() {
+    let audioList = {
+        audio1: new Audio('audio/taa-pet.mp3'),
+        audio2: new Audio('audio/jazz-club.mp3'),
+        audio3: new Audio('audio/comfort-fit.mp3'),
+        audio4: new Audio('audio/nobara.mp3'),
+        audio5: new Audio('audio/paper-navy.mp3')
+    };
 
-let audioListLen = Object.keys(audioList).length;
-let playable = document.querySelectorAll('.fas.play');
+    let audioListLen = Object.keys(audioList).length;
+    let playable = document.querySelectorAll('.fas.play');
 
-playable.forEach((item) => {
-    item.addEventListener('click', () => {
-        for (let i = 0; i < audioListLen; ++i) {
-            if (item.getAttribute('data-audio') == Object.keys(audioList)[i]) {
-                item.classList.add('hidden');
-                document.querySelector('.tool.play').classList.add('hidden');
-                document.querySelector(`.fas.pause[data-audio="${Object.keys(audioList)[i]}"]`).classList.remove('hidden');
-                document.querySelector(`.tool.pause`).classList.remove('hidden')
-                Object.values(audioList)[i].play();
+    playable.forEach((item) => {
+        item.addEventListener('click', () => {
+            for (let i = 0; i < audioListLen; ++i) {
+                if (item.getAttribute('data-audio') == Object.keys(audioList)[i]) {
+                    item.classList.add('hidden');
+                    document.querySelector('.tool.play').classList.add('hidden');
+                    document.querySelector(`.fas.pause[data-audio="${Object.keys(audioList)[i]}"]`).classList.remove('hidden');
+                    document.querySelector(`.tool.pause`).classList.remove('hidden')
+                    Object.values(audioList)[i].play();
+                }
+                let audioVal = Object.values(audioList)[i];
+                audioVal.addEventListener('timeupdate', () => {
+                    let progress = document.querySelector('progress');
+                    progress.setAttribute("value", audioVal.currentTime);
+                    progress.setAttribute("max", audioVal.duration);
+                    let currentTimeVal = document.querySelector('.progress-text .currentTime');
+                    let maxDuration = document.querySelector('.progress-text .duration');
+                    let s = (audioVal.currentTime % 60);
+                    let m = (audioVal.currentTime / 60);
+                    let durationVal = Math.floor(audioVal.duration / 60) + ":" + Math.ceil(audioVal.duration % 60);
+                    currentTimeVal.innerHTML = `${Math.floor(m)}:${Math.ceil(s)}`;
+                    maxDuration.innerHTML = durationVal;
+
+                })
             }
-            let audioVal = Object.values(audioList)[i];
-            audioVal.addEventListener('timeupdate', () => {
-                let progress = document.querySelector('progress');
-                progress.setAttribute("value", audioVal.currentTime);
-                progress.setAttribute("max", audioVal.duration);
-                let currentTimeVal = document.querySelector('.progress-text .currentTime');
-                let maxDuration = document.querySelector('.progress-text .duration');
-                let s = (audioVal.currentTime % 60);
-                let m = (audioVal.currentTime / 60);
-                let durationVal = Math.floor(audioVal.duration / 60) + ":" + Math.ceil(audioVal.duration % 60);
-                currentTimeVal.innerHTML = `${Math.floor(m)}:${Math.ceil(s)}`;
-                maxDuration.innerHTML = durationVal;
 
-            })
+        });
+
+    })
+
+    let pauseable = document.querySelectorAll('.fas.pause');
+
+    pauseable.forEach((item) => {
+        item.addEventListener('click', () => {
+            for (let i = 0; i < audioListLen; ++i) {
+                if (item.getAttribute('data-audio') == Object.keys(audioList)[i]) {
+                    item.classList.add('hidden');
+                    document.querySelector(`.tool.play`).classList.remove('hidden');
+                    document.querySelector(`.tool.play[data-audio="${Object.keys(audioList)[i]}"]`).classList.remove('hidden');
+                    document.querySelector(`.tool.pause`).classList.add('hidden');
+                    Object.values(audioList)[i].pause();
+                }
+            }
+        })
+    })
+
+    let isPlaying = false;
+    let storedVal;
+
+    let shuffle = document.querySelector('.tool.shuffle');
+    shuffle.addEventListener('click', () => {
+        if (isPlaying == false) {
+            let r = Math.floor(Math.random() * 5) + 1;
+            storedVal = r;
+            Object.values(audioList)[r].play();
+            isPlaying = true;
+        } else {
+            location.reload(); //temp cheat
         }
-
     });
 
+    let like = document.querySelector('.like');
+    like.addEventListener('click', () => {
+
+    });
 })
-
-let pauseable = document.querySelectorAll('.fas.pause');
-
-pauseable.forEach((item) => {
-    item.addEventListener('click', () => {
-        for (let i = 0; i < audioListLen; ++i) {
-            if (item.getAttribute('data-audio') == Object.keys(audioList)[i]) {
-                item.classList.add('hidden');
-                document.querySelector(`.tool.play`).classList.remove('hidden');
-                document.querySelector(`.tool.play[data-audio="${Object.keys(audioList)[i]}"]`).classList.remove('hidden');
-                document.querySelector(`.tool.pause`).classList.add('hidden');
-                Object.values(audioList)[i].pause();
-            }
-        }
-    })
-})
-
-let isPlaying = false;
-let storedVal;
-
-let shuffle = document.querySelector('.tool.shuffle');
-shuffle.addEventListener('click', () => {
-    if (isPlaying == false) {
-        let r = Math.floor(Math.random() * 5) + 1;
-        storedVal = r;
-        Object.values(audioList)[r].play();
-        isPlaying = true;
-    } else {
-        location.reload(); //temp cheat
-    }
-});
-
 
 // initial slider test
 // let audio1 = new Audio('audio/taa-pet.mp3');
